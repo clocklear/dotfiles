69e876ac2fee1ec143737e6fc6ba569ee71ff66b
Cory Locklear <clocklear@rvohealth.com>
2023-12-06 11:38:22 -0500

    Update .chezmoiignore Update .p10k.zsh

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -102,6 +102,8 @@
     # battery               # internal battery
     # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
+	shell_level
+	aws_account_alias
   )
 
   # Defines character set used by powerlevel10k. It's best to let `p10k configure` set it for you.
